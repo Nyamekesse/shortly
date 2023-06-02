766f856d7070f374d47a922b9484c8111ff526e2
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-06-02 21:20:40 +0000

    Completed hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import HeroSection from './sections/HeroSection/index.tsx'
 import NavSection from './sections/NavSection/index.tsx'
 
 function App() {
   return (
     <>
       <NavSection />
+      <HeroSection />
     </>
   )
 }
