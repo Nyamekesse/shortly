6f2b554177a5106e6f9510bcdfe19049e851706a
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-06-02 20:14:02 +0000

    initial commit, setup project structure, added images and configurations

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+      assumenda dignissimos debitis ipsam incidunt illo, et molestias rem
+      repellendus obcaecati mollitia illum autem quisquam enim sit dolore
+      provident omnis laboriosam!
+    </>
+  )
+}
+
+export default App
