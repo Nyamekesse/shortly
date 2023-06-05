5cbf666c4e4f9f8c4430fcc2fdc158c49ff36d0c
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-06-05 00:15:59 +0000

    added and completed footer and cta section

--- src/sections/NavSection/index.tsx
+++ src/sections/NavSection/index.tsx
@@ -1,7 +1,12 @@
+import React from 'react'
 import Logo from '../../assets/logo.svg'
 import NavButton from '../../components/NavButton.tsx'
 
 const NavSection = () => {
+  const [openMenu, setOpenMenu] = React.useState(false)
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu)
+  }
   return (
     <nav className="container relative mx-auto p-6">
       {/* FLEX CONTAINER ALL ITEMS */}
@@ -28,8 +33,58 @@ const NavSection = () => {
           </a>
         </div>
         {/* HAMBURGER MENU */}
+        <div
+          className="md:hidden"
+          onClick={toggleMenu}
+          role="button"
+          tabIndex={0}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter' || event.key === ' ') {
+              toggleMenu()
+            }
+          }}
+        >
+          <button
+            type="button"
+            className={`${
+              openMenu ? 'open' : ''
+            } hamburger z-40 block focus:outline-none md:hidden`}
+            id="btnMenu"
+          >
+            <span className="hamburger-top" />
+            <span className="hamburger-middle" />
+            <span className="hamburger-bottom" />
+          </button>
+        </div>
       </div>
       {/* MOBILE MENU */}
+      <div
+        id="menu"
+        className={`z-100 absolute left-6 right-6 top-20 ${
+          openMenu ? 'flex' : 'hidden'
+        } rounded-lg bg-darkViolet p-6`}
+      >
+        <div className="flex w-full flex-col items-center justify-center space-y-6 rounded-sm font-bold text-white">
+          <a href="#" className="w-full text-center">
+            Features
+          </a>
+          <a href="#" className="w-full text-center">
+            Pricing
+          </a>
+          <a href="#" className="w-full text-center">
+            Resources
+          </a>
+          <a
+            href="#"
+            className="w-full border-t border-gray-400 pt-6 text-center"
+          >
+            LogIn
+          </a>
+          <a href="#" className="w-full rounded-full bg-cyan py-3 text-center">
+            Sign Up
+          </a>
+        </div>
+      </div>
     </nav>
   )
 }
